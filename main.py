290d834c4853f7a6eedfde59430dab3dc0667e21
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2025-06-14 19:06:20 -0600

    reorganize and shift design style

--- main.py
+++ main.py
@@ -7,18 +7,8 @@ pg.init()
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 900, 600
 displaySurface = pg.display.set_mode(SCREEN_SIZE)
 
-import module.node as Node
-
 clock = pg.Clock()
 
-nodes = Node.NodeGroup()
-
-def determine_visible_nodes(nodes: Node.NodeGroup):
-    for node in nodes.sprites():
-        if not node.hidden:
-            continue
-
-        node.visible = dist(mouse_pos, node.pos) < node.show_radius
 
 running = True
 while running:
@@ -31,18 +21,18 @@ while running:
         if event.type == QUIT:
             running = False
         if event.type == MOUSEBUTTONUP:
-            if event.button == 1:
-                nodes.add(Node.Empty(mouse_pos, True))
-            if event.button == 3:
-                nodes.add(Node.Empty(mouse_pos, False))
+            # mine from ground
+            ...
+            # if event.button == 1:
+            #     nodes.add(Node.Empty(mouse_pos, True))
+            # if event.button == 3:
+            #     nodes.add(Node.Empty(mouse_pos, False))
     
     # logic
-    determine_visible_nodes(nodes)
 
     # rendering
     displaySurface.fill((0, 0, 0))
 
-    nodes.draw_visible(displaySurface)
 
     pg.display.update()
 
