e20f3665eb26b67ad458bf8e593d36c8e9badbf4
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2025-06-14 18:28:40 -0600

    hiding/showing nodes on hover

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pygame as pg
 from pygame.locals import *
 from sys import exit
+from math import dist
 
 pg.init()
-
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 900, 600
 displaySurface = pg.display.set_mode(SCREEN_SIZE)
+
+import module.node as Node
+
 clock = pg.Clock()
+
+nodes = Node.NodeGroup()
+
+def determine_visible_nodes(nodes: Node.NodeGroup):
+    for node in nodes.sprites():
+        if not node.hidden:
+            continue
+
+        node.visible = dist(mouse_pos, node.pos) < node.show_radius
 
 running = True
 while running:
@@ -18,11 +30,19 @@ while running:
     for event in pg.event.get():
         if event.type == QUIT:
             running = False
+        if event.type == MOUSEBUTTONUP:
+            if event.button == 1:
+                nodes.add(Node.Empty(mouse_pos, True))
+            if event.button == 3:
+                nodes.add(Node.Empty(mouse_pos, False))
     
     # logic
+    determine_visible_nodes(nodes)
 
     # rendering
     displaySurface.fill((0, 0, 0))
+
+    nodes.draw_visible(displaySurface)
 
     pg.display.update()
 
--- module/node.py
+++ module/node.py
@@ -1,14 +1,25 @@
-from pygame import sprite, image, Surface, Vector2
+from pygame import sprite, image, Surface, Vector2, transform
 
 class NodeType(sprite.Sprite):
-    surface: Surface
-    number: int
+    image: Surface
+    show_radius: int = 20
 
 class Empty(NodeType):
-    print("loading image")
-    surface = image.load(r"asset\node\blank.png").convert_alpha()
-    def __init__(self, pos: Vector2 | tuple[int, int]):
+    surface = transform.scale(image.load(r"asset\node\blank.png").convert_alpha(), (16, 16))
+    def __init__(self, pos: Vector2 | tuple[int, int], hidden: bool = True):
         super().__init__()
-        self.surface = Empty.surface.copy()
-        self.rect = Empty.surface.get_rect()
+        self.image = Empty.surface.copy()
         self.pos = Vector2(pos)
+        self.rect = Empty.surface.get_rect(center=pos)
+
+        # determines if the node will only show on mouse over (true) or always show (false)
+        self.hidden = hidden
+        # determines if the node is currently visible
+        self.visible: bool = not self.hidden 
+
+class NodeGroup(sprite.Group):
+    def draw_visible(self, surface: Surface):
+        for spr in self.sprites():
+            if hasattr(spr, "visible") and not spr.visible:
+                continue
+            surface.blit(spr.image, spr.rect)
