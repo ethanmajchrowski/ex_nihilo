f01a452c1432e6836a4c429163ee1abbeffa9ede
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2025-07-14 11:29:53 -0600

    game entry point

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
+from logger import logger
+import pygame as pg
+import data.configuration as c
 
+from game.game import Game
 
 def main():
-    pass
+    pg.init()
+    display_surface = pg.display.set_mode(c.DISPLAY_SIZE)
+    pg.display.set_caption("EX NIHILO | FPS: 0")
+    logger.info("Game initialized")
+    game = Game(display_surface)
+    game.run()
 
 if __name__ == "__main__":
     main()
