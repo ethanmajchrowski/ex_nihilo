ab304ca26f577ec6dc319ce6c94e1bf0c7522929
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2025-07-14 12:15:49 -0600

    made entity_manager global

---
+++ src/core/entity_manager.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from game.machine import Machine
+
+class _EntityManager:
+    def __init__(self) -> None:
+        self.entities = set()
+    
+    def add_entity(self, entity):
+        self.entities.add(entity)
+
+    def remove_entity(self, entity):
+        self.entities.remove(entity)
+    
+    def get_tickable_entities(self):
+        return [e for e in self.entities if hasattr(e, "tick")]
+    
+    def get_machines(self):
+        return [e for e in self.entities if isinstance(e, Machine)]
+
+entity_manager = _EntityManager()
