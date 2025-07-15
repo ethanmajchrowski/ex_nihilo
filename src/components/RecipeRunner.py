5e459563f89e6a5f6791cca7f0142cf6213eb607
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2025-07-15 15:34:45 -0600

    more recipe runner work: added conditions for item quantities in machine input nodes

--- src/components/RecipeRunner.py
+++ src/components/RecipeRunner.py
@@ -3,6 +3,7 @@ import data.configuration as c
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from core.recipe_registry import Recipe
+    from components.ionode import ItemIONode
 
 class RecipeRunner(BaseComponent):
     def __init__(self, parent, args) -> None:
@@ -12,13 +13,50 @@ class RecipeRunner(BaseComponent):
         self.selected_recipe: Recipe | None = None
         self.progress = 0
         self.progress_pct = 0.0
+    
+    def can_start_recipe(self) -> bool:
+        if not self.parent.enabled:
+            return False
+        if not self.parent.can_run():
+            return False
+        if not self.selected_recipe:
+            return False
         
+        # if a recipe is selected we assume that the machine has the capabilities to run it:
+        #   - the machine has the required IOnodes to run it (e.g. number of input/output)
+        #   - the machine has the required capability tags
+        
+        # check item quantities in IOnodes
+        recipe_inputs = self.selected_recipe.inputs
+        input_nodes: list[ItemIONode] = [n for n in self.parent.nodes if n.node_type == "input" and n.kind == "item"]
+        accounted_items: dict[str, int] = {}
+        
+        for item, amount in recipe_inputs.items():
+            for node in input_nodes:
+                if not node.item or node.item != item:
+                    continue
+                accounted_items.update({node.item: node.quantity})
+            
+            if accounted_items[item] < amount:
+                print(f"not enough {item} to run {self.selected_recipe.name} ({accounted_items[item]}/{amount})")
+                return False
+    
+        # todo: ensure valid output space for recipe outputs
+
+        return True
+    
     def tick(self):
-        if not self.parent.enabled or not self.parent.can_run() or not self.selected_recipe:
+        just_started = self.is_running
+        if not self.can_start_recipe():
             self.is_running = False
             self.progress = 0
             return
+        
+        assert self.selected_recipe
+        
         self.is_running = True
+        if not just_started and self.is_running:
+            self.start_recipe()
         
         ticks_required = self.selected_recipe.duration * c.SIMULATION_TICKS_PER_SECOND
         
@@ -27,9 +65,20 @@ class RecipeRunner(BaseComponent):
         if self.progress >= ticks_required:
             self.progress = 0
             self.complete_recipe()
+            self.is_running = False
     
     def complete_recipe(self):
         if self.selected_recipe:
             print(f"{self.selected_recipe.name} complete!")
+            # todo: remove items from input nodes
+            # recipe_inputs = self.selected_recipe.inputs
+            # input_nodes: list[ItemIONode] = [n for n in self.parent.nodes if n.node_type == "input" and n.kind == "item"]
+    
+    def start_recipe(self):
+        if self.selected_recipe:
+            print(f"{self.selected_recipe.name} started!")
+            # todo: distribute output items across output nodes
+            # recipe_outputs = self.selected_recipe.outputs
+            # output_nodes: list[ItemIONode] = [n for n in self.parent.nodes if n.node_type == "output" and n.kind == "item"]
         
         
--- src/components/ionode.py
+++ src/components/ionode.py
@@ -5,25 +5,24 @@ if TYPE_CHECKING:
     from game.machine import Machine
 
 class IONode:
-    def __init__(self, 
-                 parent_machine: Machine, 
-                 node_type: Literal["input", "output"], 
-                 offset: tuple[float, float]) -> None:
-        """Base class for IONodes.
-
-        Args:
-            parent_machine (_type_): Machine that this node is connected to.
-            node_type ("input" or "output"): Node direction
-            offset (tuple[float, float]): Position of node relative to machine, in terms of machine size.
-        """
+    def __init__(self, parent_machine: "Machine", node_type: Literal["input", "output"], offset: tuple[float, float]) -> None:
         self.parent = parent_machine
         self.node_type = node_type
         self.offset = offset
+        self.kind: Literal["item", "energy"]
         
         io_registry.register(self.calculate_abs_pos(), self)
+        self.abs_pos = self.calculate_abs_pos()
     
     def calculate_abs_pos(self):
         return (
             self.parent.position[0] + c.BASE_MACHINE_HEIGHT * self.offset[0] / 2,
             self.parent.position[1] + c.BASE_MACHINE_WIDTH * self.offset[1] / 2
         )
+
+class ItemIONode(IONode):
+    def __init__(self, parent_machine: "Machine", node_type: Literal['input'] | Literal['output'], offset: tuple[float, float]) -> None:
+        super().__init__(parent_machine, node_type, offset)
+        self.item: None | str = None
+        self.quantity = 0
+        self.kind = "item"
